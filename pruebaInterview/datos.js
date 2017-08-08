587ca582f17b064713742e2d97d4022e24c01237
DESKTOP-9UAUI7P\Chava <salvadorgonmo@gmail.com>
2017-08-07 22:19:28 -0500

    Anexando funcion del click con extraccion de datos en un vector (array) y mandando el promedio en un alert :)

---
+++ pruebaInterview/datos.js
@@ -0,0 +1,9 @@
+function hacerPromedio(){
+ var items = document.querySelectorAll("#coleccion");
+ var suma = 0;
+ for(var i=0;i<items.length;i++){
+   suma = suma + parseInt(items[i].innerHTML);
+ }
+  var resultado = suma / 9;
+  alert('Tu promedio es: '+ resultado);
+}
